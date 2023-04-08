895e90fc5433977de53c2814741e34bcf27d07b1
Bruno <brunomartins1030@gmail.com>
2023-04-08 11:36:14 -0300

    first commit

---
+++ src/main/java/com/bruno/apachecamelproject/controller/StateController.java
@@ -0,0 +1,24 @@
+package com.bruno.apachecamelproject.controller;
+
+import com.bruno.apachecamelproject.service.StateService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("state")
+@AllArgsConstructor
+public class StateController {
+
+    private StateService service;
+
+    @GetMapping("/{chave}")
+    public Map<String, String> getAll(@PathVariable String chave){
+        return service.getAll(chave);
+    }
+
+}
---
+++ src/main/java/com/bruno/apachecamelproject/service/StateService.java
@@ -0,0 +1,14 @@
+package com.bruno.apachecamelproject.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class StateService {
+
+    public Map<String, String> getAll(String chave){
+        return null;
+    }
+
+}
