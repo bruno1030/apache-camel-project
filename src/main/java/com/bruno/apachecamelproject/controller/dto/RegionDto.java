87f034c2cfd9888a3b1959a358aaa516871e5974
Bruno <brunomartins1030@gmail.com>
2023-04-10 15:37:32 -0300

    add ObjectMapper

--- src/main/java/com/bruno/apachecamelproject/controller/dto/RegionDto.java
+++ src/main/java/com/bruno/apachecamelproject/controller/dto/RegionDto.java
@@ -2,15 +2,9 @@ package com.bruno.apachecamelproject.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class StateDto {
+public class RegionDto {
 
     @JsonProperty("nome")
     private String name;
-
-    @JsonProperty("sigla")
-    private String acronym;
-
-    @JsonProperty("regiao")
-    private RegionDto Region;
 
 }
--- src/main/java/com/bruno/apachecamelproject/repository/impl/StateRepositoryImpl.java
+++ src/main/java/com/bruno/apachecamelproject/repository/impl/StateRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.bruno.apachecamelproject.repository.impl;
 
 import com.bruno.apachecamelproject.controller.dto.StateDto;
 import com.bruno.apachecamelproject.repository.StateRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.net.URI;
@@ -14,26 +15,33 @@ import java.util.List;
 
 public class StateRepositoryImpl implements StateRepository {
 
-    // URL (1/4)
+    // URL (1/6)
     public static final String URL_GET = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
+
+    ObjectMapper mapper;
 
     @Override
     public List<StateDto> getStates() throws IOException, InterruptedException {
 
         List<StateDto> stateDtoList = new ArrayList<>();
 
-        // http client (2/4)
+        // http client (2/6)
         HttpClient client = HttpClient.newHttpClient();
 
-        // request (3/4)
+        // request (3/6)
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .timeout(Duration.ofSeconds(10))
                 .uri(URI.create(URL_GET))
                 .build();
 
-        // response (4/4)
+        // response (4/6)
         HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        // get the response.body
+        String body = response.body();
+
+        StateDto dtoList = mapper.readValue(body, StateDto.class);
 
         return null;
     }
